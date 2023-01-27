6758ce6dbe1bd161ea7e26be1865bb7d9e2e3bf8
anaszil <anas.zilali@outlook.com>
2023-01-27 20:30:50 +0100

    adapt to kaggle

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 import torch
 import os
 from torch.utils.data import Dataset
+from glob import glob
 
 
 class CustomDataset(Dataset):
@@ -18,15 +19,37 @@ class CustomDataset(Dataset):
         self.df = df
         self.epoch_len = self.df.shape[0]
         self.sep = "/"
+        if hasattr(self.cfg, "kaggle") and self.cfg.kaggle == True:
+            self.path_skip = "/content/drive/MyDrive/RSNA/Data/PNG/png_full_size_train"
+            self.chunk1 = set(
+                glob("/kaggle/input/rsna-png-chunk1/" + self.path_skip + "/*")
+            )
+            # self.chunk2 = set(glob("/kaggle/input/rsna-png-chunk2/" + self.path_skip + "/*"))
 
         if hasattr(self.cfg, "sep_data_path"):
             self.sep = self.cfg.sep_data_path
 
     def __getitem__(self, idx):
         sample = self.df.iloc[idx]
-        img_path = os.path.join(
-            self.cfg.root_dir, f"{sample.patient_id}{self.sep}{sample.image_id}.png"
-        )
+
+        if hasattr(self.cfg, "kaggle") and self.cfg.kaggle == True:
+            if sample.patient_id in self.chunk1:
+                img_path = os.path.join(
+                    self.cfg.root_dir + "1",
+                    self.path_skip,
+                    f"{sample.patient_id}{self.sep}{sample.image_id}.png",
+                )
+            else:
+                img_path = os.path.join(
+                    self.cfg.root_dir + "2",
+                    self.path_skip,
+                    f"{sample.patient_id}{self.sep}{sample.image_id}.png",
+                )
+        else:
+            img_path = os.path.join(
+                self.cfg.root_dir, f"{sample.patient_id}{self.sep}{sample.image_id}.png"
+            )
+
         label = np.expand_dims(np.array(sample.cancer, dtype=np.int8), axis=0)
 
         data = {
