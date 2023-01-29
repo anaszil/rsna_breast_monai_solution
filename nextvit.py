3a8e053e6d3bed0de1548b03e1f6e18273ff056e
anaszil <anas.zilali@outlook.com>
2023-01-29 02:07:22 +0100

    add nextvit

--- nextvit.py
+++ nextvit.py
@@ -7,22 +7,23 @@ from einops import rearrange
 from timm.models.layers import DropPath, trunc_normal_
 from timm.models.registry import register_model
 from torch import nn
-from utils import merge_pre_bn
+from utils_nextvit import merge_pre_bn
 
 NORM_EPS = 1e-5
 
 
 class ConvBNReLU(nn.Module):
-    def __init__(
-            self,
+    def __init__(self, in_channels, out_channels, kernel_size, stride, groups=1):
+        super(ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(
             in_channels,
             out_channels,
-            kernel_size,
-            stride,
-            groups=1):
-        super(ConvBNReLU, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
-                              padding=1, groups=groups, bias=False)
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=1,
+            groups=groups,
+            bias=False,
+        )
         self.norm = nn.BatchNorm2d(out_channels, eps=NORM_EPS)
         self.act = nn.ReLU(inplace=True)
 
@@ -44,19 +45,22 @@ def _make_divisible(v, divisor, min_value=None):
 
 
 class PatchEmbed(nn.Module):
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 stride=1):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(PatchEmbed, self).__init__()
         norm_layer = partial(nn.BatchNorm2d, eps=NORM_EPS)
         if stride == 2:
-            self.avgpool = nn.AvgPool2d((2, 2), stride=2, ceil_mode=True, count_include_pad=False)
-            self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)
+            self.avgpool = nn.AvgPool2d(
+                (2, 2), stride=2, ceil_mode=True, count_include_pad=False
+            )
+            self.conv = nn.Conv2d(
+                in_channels, out_channels, kernel_size=1, stride=1, bias=False
+            )
             self.norm = norm_layer(out_channels)
         elif in_channels != out_channels:
             self.avgpool = nn.Identity()
-            self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)
+            self.conv = nn.Conv2d(
+                in_channels, out_channels, kernel_size=1, stride=1, bias=False
+            )
             self.norm = norm_layer(out_channels)
         else:
             self.avgpool = nn.Identity()
@@ -71,14 +75,24 @@ class MHCA(nn.Module):
     """
     Multi-Head Convolutional Attention
     """
+
     def __init__(self, out_channels, head_dim):
         super(MHCA, self).__init__()
         norm_layer = partial(nn.BatchNorm2d, eps=NORM_EPS)
-        self.group_conv3x3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1,
-                                       padding=1, groups=out_channels // head_dim, bias=False)
+        self.group_conv3x3 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            groups=out_channels // head_dim,
+            bias=False,
+        )
         self.norm = norm_layer(out_channels)
         self.act = nn.ReLU(inplace=True)
-        self.projection = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False)
+        self.projection = nn.Conv2d(
+            out_channels, out_channels, kernel_size=1, bias=False
+        )
 
     def forward(self, x):
         out = self.group_conv3x3(x)
@@ -89,7 +103,9 @@ class MHCA(nn.Module):
 
 
 class Mlp(nn.Module):
-    def __init__(self, in_features, out_features=None, mlp_ratio=None, drop=0., bias=True):
+    def __init__(
+        self, in_features, out_features=None, mlp_ratio=None, drop=0.0, bias=True
+    ):
         super().__init__()
         out_features = out_features or in_features
         hidden_dim = _make_divisible(in_features * mlp_ratio, 32)
@@ -114,8 +130,17 @@ class NCB(nn.Module):
     """
     Next Convolution Block
     """
-    def __init__(self, in_channels, out_channels, stride=1, path_dropout=0,
-                 drop=0, head_dim=32, mlp_ratio=3):
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        stride=1,
+        path_dropout=0,
+        drop=0,
+        head_dim=32,
+        mlp_ratio=3,
+    ):
         super(NCB, self).__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -151,13 +176,23 @@ class E_MHSA(nn.Module):
     """
     Efficient Multi-Head Self Attention
     """
-    def __init__(self, dim, out_dim=None, head_dim=32, qkv_bias=True, qk_scale=None,
-                 attn_drop=0, proj_drop=0., sr_ratio=1):
+
+    def __init__(
+        self,
+        dim,
+        out_dim=None,
+        head_dim=32,
+        qkv_bias=True,
+        qk_scale=None,
+        attn_drop=0,
+        proj_drop=0.0,
+        sr_ratio=1,
+    ):
         super().__init__()
         self.dim = dim
         self.out_dim = out_dim if out_dim is not None else dim
         self.num_heads = self.dim // head_dim
-        self.scale = qk_scale or head_dim ** -0.5
+        self.scale = qk_scale or head_dim**-0.5
         self.q = nn.Linear(dim, self.dim, bias=qkv_bias)
         self.k = nn.Linear(dim, self.dim, bias=qkv_bias)
         self.v = nn.Linear(dim, self.dim, bias=qkv_bias)
@@ -166,7 +201,7 @@ class E_MHSA(nn.Module):
         self.proj_drop = nn.Dropout(proj_drop)
 
         self.sr_ratio = sr_ratio
-        self.N_ratio = sr_ratio ** 2
+        self.N_ratio = sr_ratio**2
         if sr_ratio > 1:
             self.sr = nn.AvgPool1d(kernel_size=self.N_ratio, stride=self.N_ratio)
             self.norm = nn.BatchNorm1d(dim, eps=NORM_EPS)
@@ -185,7 +220,9 @@ class E_MHSA(nn.Module):
     def forward(self, x):
         B, N, C = x.shape
         q = self.q(x)
-        q = q.reshape(B, N, self.num_heads, int(C // self.num_heads)).permute(0, 2, 1, 3)
+        q = q.reshape(B, N, self.num_heads, int(C // self.num_heads)).permute(
+            0, 2, 1, 3
+        )
 
         if self.sr_ratio > 1:
             x_ = x.transpose(1, 2)
@@ -194,14 +231,22 @@ class E_MHSA(nn.Module):
                 x_ = self.norm(x_)
             x_ = x_.transpose(1, 2)
             k = self.k(x_)
-            k = k.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(0, 2, 3, 1)
+            k = k.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(
+                0, 2, 3, 1
+            )
             v = self.v(x_)
-            v = v.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(0, 2, 1, 3)
+            v = v.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(
+                0, 2, 1, 3
+            )
         else:
             k = self.k(x)
-            k = k.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(0, 2, 3, 1)
+            k = k.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(
+                0, 2, 3, 1
+            )
             v = self.v(x)
-            v = v.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(0, 2, 1, 3)
+            v = v.reshape(B, -1, self.num_heads, int(C // self.num_heads)).permute(
+                0, 2, 1, 3
+            )
         attn = (q @ k) * self.scale
 
         attn = attn.softmax(dim=-1)
@@ -217,9 +262,19 @@ class NTB(nn.Module):
     """
     Next Transformer Block
     """
+
     def __init__(
-            self, in_channels, out_channels, path_dropout, stride=1, sr_ratio=1,
-            mlp_ratio=2, head_dim=32, mix_block_ratio=0.75, attn_drop=0, drop=0,
+        self,
+        in_channels,
+        out_channels,
+        path_dropout,
+        stride=1,
+        sr_ratio=1,
+        mlp_ratio=2,
+        head_dim=32,
+        mix_block_ratio=0.75,
+        attn_drop=0,
+        drop=0,
     ):
         super(NTB, self).__init__()
         self.in_channels = in_channels
@@ -227,16 +282,25 @@ class NTB(nn.Module):
         self.mix_block_ratio = mix_block_ratio
         norm_func = partial(nn.BatchNorm2d, eps=NORM_EPS)
 
-        self.mhsa_out_channels = _make_divisible(int(out_channels * mix_block_ratio), 32)
+        self.mhsa_out_channels = _make_divisible(
+            int(out_channels * mix_block_ratio), 32
+        )
         self.mhca_out_channels = out_channels - self.mhsa_out_channels
 
         self.patch_embed = PatchEmbed(in_channels, self.mhsa_out_channels, stride)
         self.norm1 = norm_func(self.mhsa_out_channels)
-        self.e_mhsa = E_MHSA(self.mhsa_out_channels, head_dim=head_dim, sr_ratio=sr_ratio,
-                             attn_drop=attn_drop, proj_drop=drop)
+        self.e_mhsa = E_MHSA(
+            self.mhsa_out_channels,
+            head_dim=head_dim,
+            sr_ratio=sr_ratio,
+            attn_drop=attn_drop,
+            proj_drop=drop,
+        )
         self.mhsa_path_dropout = DropPath(path_dropout * mix_block_ratio)
 
-        self.projection = PatchEmbed(self.mhsa_out_channels, self.mhca_out_channels, stride=1)
+        self.projection = PatchEmbed(
+            self.mhsa_out_channels, self.mhca_out_channels, stride=1
+        )
         self.mhca = MHCA(self.mhca_out_channels, head_dim=head_dim)
         self.mhca_path_dropout = DropPath(path_dropout * (1 - mix_block_ratio))
 
@@ -276,22 +340,37 @@ class NTB(nn.Module):
 
 
 class NextViT(nn.Module):
-    def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
-                 strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
-                 use_checkpoint=False):
+    def __init__(
+        self,
+        stem_chs,
+        depths,
+        path_dropout,
+        attn_drop=0,
+        drop=0,
+        num_classes=1000,
+        strides=[1, 2, 2, 2],
+        sr_ratios=[8, 4, 2, 1],
+        head_dim=32,
+        mix_block_ratio=0.75,
+        use_checkpoint=False,
+    ):
         super(NextViT, self).__init__()
         self.use_checkpoint = use_checkpoint
 
-        self.stage_out_channels = [[96] * (depths[0]),
-                                   [192] * (depths[1] - 1) + [256],
-                                   [384, 384, 384, 384, 512] * (depths[2] // 5),
-                                   [768] * (depths[3] - 1) + [1024]]
+        self.stage_out_channels = [
+            [96] * (depths[0]),
+            [192] * (depths[1] - 1) + [256],
+            [384, 384, 384, 384, 512] * (depths[2] // 5),
+            [768] * (depths[3] - 1) + [1024],
+        ]
 
         # Next Hybrid Strategy
-        self.stage_block_types = [[NCB] * depths[0],
-                                  [NCB] * (depths[1] - 1) + [NTB],
-                                  [NCB, NCB, NCB, NCB, NTB] * (depths[2] // 5),
-                                  [NCB] * (depths[3] - 1) + [NTB]]
+        self.stage_block_types = [
+            [NCB] * depths[0],
+            [NCB] * (depths[1] - 1) + [NTB],
+            [NCB, NCB, NCB, NCB, NTB] * (depths[2] // 5),
+            [NCB] * (depths[3] - 1) + [NTB],
+        ]
 
         self.stem = nn.Sequential(
             ConvBNReLU(3, stem_chs[0], kernel_size=3, stride=2),
@@ -302,7 +381,9 @@ class NextViT(nn.Module):
         input_channel = stem_chs[-1]
         features = []
         idx = 0
-        dpr = [x.item() for x in torch.linspace(0, path_dropout, sum(depths))]  # stochastic depth decay rule
+        dpr = [
+            x.item() for x in torch.linspace(0, path_dropout, sum(depths))
+        ]  # stochastic depth decay rule
         for stage_id in range(len(depths)):
             numrepeat = depths[stage_id]
             output_channels = self.stage_out_channels[stage_id]
@@ -315,13 +396,27 @@ class NextViT(nn.Module):
                 output_channel = output_channels[block_id]
                 block_type = block_types[block_id]
                 if block_type is NCB:
-                    layer = NCB(input_channel, output_channel, stride=stride, path_dropout=dpr[idx + block_id],
-                                drop=drop, head_dim=head_dim)
+                    layer = NCB(
+                        input_channel,
+                        output_channel,
+                        stride=stride,
+                        path_dropout=dpr[idx + block_id],
+                        drop=drop,
+                        head_dim=head_dim,
+                    )
                     features.append(layer)
                 elif block_type is NTB:
-                    layer = NTB(input_channel, output_channel, path_dropout=dpr[idx + block_id], stride=stride,
-                                sr_ratio=sr_ratios[stage_id], head_dim=head_dim, mix_block_ratio=mix_block_ratio,
-                                attn_drop=attn_drop, drop=drop)
+                    layer = NTB(
+                        input_channel,
+                        output_channel,
+                        path_dropout=dpr[idx + block_id],
+                        stride=stride,
+                        sr_ratio=sr_ratios[stage_id],
+                        head_dim=head_dim,
+                        mix_block_ratio=mix_block_ratio,
+                        attn_drop=attn_drop,
+                        drop=drop,
+                    )
                     features.append(layer)
                 input_channel = output_channel
             idx += numrepeat
@@ -334,8 +429,8 @@ class NextViT(nn.Module):
             nn.Linear(output_channel, num_classes),
         )
 
-        self.stage_out_idx = [sum(depths[:idx + 1]) - 1 for idx in range(len(depths))]
-        print('initialize_weights...')
+        self.stage_out_idx = [sum(depths[: idx + 1]) - 1 for idx in range(len(depths))]
+        print("initialize_weights...")
         self._initialize_weights()
 
     def merge_bn(self):
@@ -346,16 +441,18 @@ class NextViT(nn.Module):
 
     def _initialize_weights(self):
         for n, m in self.named_modules():
-            if isinstance(m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)):
+            if isinstance(
+                m, (nn.BatchNorm2d, nn.GroupNorm, nn.LayerNorm, nn.BatchNorm1d)
+            ):
                 nn.init.constant_(m.weight, 1.0)
                 nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if hasattr(m, 'bias') and m.bias is not None:
+                trunc_normal_(m.weight, std=0.02)
+                if hasattr(m, "bias") and m.bias is not None:
                     nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.Conv2d):
-                trunc_normal_(m.weight, std=.02)
-                if hasattr(m, 'bias') and m.bias is not None:
+                trunc_normal_(m.weight, std=0.02)
+                if hasattr(m, "bias") and m.bias is not None:
                     nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
@@ -374,17 +471,23 @@ class NextViT(nn.Module):
 
 @register_model
 def nextvit_small(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 10, 3], path_dropout=0.1, **kwargs)
+    model = NextViT(
+        stem_chs=[64, 32, 64], depths=[3, 4, 10, 3], path_dropout=0.1, **kwargs
+    )
     return model
 
 
 @register_model
 def nextvit_base(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 20, 3], path_dropout=0.2, **kwargs)
+    model = NextViT(
+        stem_chs=[64, 32, 64], depths=[3, 4, 20, 3], path_dropout=0.2, **kwargs
+    )
     return model
 
 
 @register_model
 def nextvit_large(pretrained=False, pretrained_cfg=None, **kwargs):
-    model = NextViT(stem_chs=[64, 32, 64], depths=[3, 4, 30, 3], path_dropout=0.2, **kwargs)
+    model = NextViT(
+        stem_chs=[64, 32, 64], depths=[3, 4, 30, 3], path_dropout=0.2, **kwargs
+    )
     return model
